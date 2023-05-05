2b79694555be8003ae55cc4762dd017d95412cb0
Joan López Ferrer <trombonet12@gmail.com>
2023-05-05 19:22:01 +0200

    🤑

--- assets/js/supermercats.js
+++ assets/js/supermercats.js
@@ -1,6 +1,6 @@
 async function llistaSupermercats() {
     try{
-        const response = await fetch('supermercat.json');
+        const response = await fetch('/assets/json/supermercats.json');
         const json = await response.json();
         const itemList = json.itemListElement;
         const container = document.querySelector('.supermercats-container');
