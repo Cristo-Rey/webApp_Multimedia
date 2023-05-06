5001dd188610aaf2e92bb70d29b616b6eced7fc0
Xavier Vives Marcus <83074289+AtomicLettuce@users.noreply.github.com>
2023-05-06 18:01:08 +0200

    el mapa el mapa elmapa mapa

---
+++ assets/js/mapa.js
@@ -0,0 +1,24 @@
+let map = L.map('map').setView([39.6136200, 3.0200400],9)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var singleMarker = L.marker([39.6136200, 3.0200400]);
+singleMarker.addTo(map);
+
+
+try{
+    const response = await fetch('/assets/js/supermercat.json');
+    const json = await response.json();
+    const itemList = json.itemListElement;
+    const container = document.querySelector('.supermercats-container');
+    for (let i = 0; i < itemList.length; i++) {
+        const item = itemList[i];
+        var singleMarker = L.marker([item.geo.latitude, item.geo.longitude]);
+        singleMarker.addTo(map);
+    }
+}
+catch(error){
+    console.error('Hubo un error al cargar el archivo JSON', error);
+}
