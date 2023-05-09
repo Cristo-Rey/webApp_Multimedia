c4d3d8ec8c7ef8353e104d664e7913dcf098db11
Xavier Vives Marcus <83074289+AtomicLettuce@users.noreply.github.com>
2023-05-09 11:39:07 +0200

    me da pánico mi propio ego, ciego de mi mismo

--- assets/js/mapa.js
+++ assets/js/mapa.js
@@ -6,7 +6,57 @@ async function fer_mapa() {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    var singleMarker = L.marker([39.6136200, 3.0200400]);
+    var greenIcon = L.icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var redIcon = L.icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var blueIcon = L.icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var yellowIcon = L.icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var orangeIcon = L.icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var blackIcon = L.icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+
+    var defaultIcon = L.icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var singleMarker = L.marker([39.6136200, 3.0200400], { icon: greenIcon });
     singleMarker.addTo(map);
 
 
@@ -17,6 +67,33 @@ async function fer_mapa() {
         const container = document.querySelector('.supermercats-container');
         for (let i = 0; i < itemList.length; i++) {
             const item = itemList[i];
+
+            switch (item.brand.name) {
+                case "Eroski":
+                    var singleMarker = L.marker([item.geo.latitude, item.geo.longitude], { icon: redIcon });
+                    break;
+                case "Mercadona":
+                    var singleMarker = L.marker([item.geo.latitude, item.geo.longitude], { icon: orangeIcon });
+                    break;
+                case "Carrefour":
+                    var singleMarker = L.marker([item.geo.latitude, item.geo.longitude], { icon: blueIcon });
+                    break;
+                case "Lidl":
+                    var singleMarker = L.marker([item.geo.latitude, item.geo.longitude], { icon: yellowIcon });
+                    break;
+                case "BipBip":
+                    var singleMarker = L.marker([item.geo.latitude, item.geo.longitude], { icon: greenIcon });
+                    break;
+                case "Aprop":
+                    var singleMarker = L.marker([item.geo.latitude, item.geo.longitude], { icon: blackIcon });
+                    break;
+                default:
+                    var singleMarker = L.marker([item.geo.latitude, item.geo.longitude], { icon: defaultIcon });
+                    break;
+
+            }
+            singleMarker.addTo(map);
+
             var singleMarker = L.marker([item.geo.latitude, item.geo.longitude]);
             singleMarker.addTo(map);
         }
