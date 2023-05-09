83167b4680da2173aed21a50813bb8520501c6fb
Xavier Vives Marcus <xvivesm@gmail.com>
2023-05-09 18:27:20 +0200

    mapa¿8

--- assets/js/sucursal.js
+++ assets/js/sucursal.js
@@ -144,9 +144,6 @@ async function loadSucursal() {
                 var singleMarker = L.marker([item.geo.latitude, item.geo.longitude], { icon: xinxeta });
                 singleMarker.addTo(map);*/
 
-                // Afegim el mapa al container
-                container3.appendChild(map._container);
-
 
                 // Salir del bucle cuando se encuentra el elemento buscado
                 break;
