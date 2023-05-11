564fbbfc48e894e254c02ffd4cc5c0ac4889c9ac
Joan López Ferrer <trombonet12@gmail.com>
2023-05-11 09:20:39 +0200

    amem ara

--- assets/js/mapa.js
+++ assets/js/mapa.js
@@ -94,8 +94,8 @@ async function fer_mapa() {
             // Crear el popup con la información del supermercado
             const popupContent = `
                 <div>
-                <h2>${item.name}</h2>
-                <img src="${item.image}" style="width: 100%;" onclick=enviarSucursal(${item.name})>
+                <h2 style="font-size: 10px;";>${item.name}</h2>
+                <img src="${item.image}" style="width: 50%;" onclick=enviarSucursal('${item.name}')>
                 <p><a href='sucursal.html' target="_blank">Més informació</a></p>
                 </div>
             `;
