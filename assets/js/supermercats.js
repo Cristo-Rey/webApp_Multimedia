0c8b7553260286d25ee7fbec863c882f562e72b3
Xavier Vives Marcus <xvivesm@gmail.com>
2023-05-16 16:50:46 +0200

    con pocos bombos compongo pongo troncos impongo dipotongos

--- assets/js/supermercats.js
+++ assets/js/supermercats.js
@@ -1,7 +1,50 @@
 async function llistaSupermercats() {
-    try{
-        const response = await fetch('/assets/js/supermercat.json');
-        const json = await response.json();
+    try {
+        const responseFrnaquicies = await fetch('/assets/js/franquicies.json');
+        const jsonFranquicies = await responseFrnaquicies.json();
+        const itemList = json.itemListElement;
+        const llista_franquicies = document.getElementById('llista_franquicies');
+        const div_franquicies = document.getElementById('div_franquicies');
+
+        const liElement = document.createElement('li');
+        liElement.classList.add('nav-item','active','show');
+        const aElement = document.createElement('a');
+        aElement.classList.add('nav-link');
+        aElement.setAttribute('data-bs-toggle', 'tab');
+        aElement.setAttribute('href', '#tab-0');
+        aElement.textContent = itemList[0].name;
+        liElement.appendChild(aElement);
+        llista_franquicies.appendChild(liElement);
+
+
+
+        for (let i = 0; i < itemList.length; i++) {
+            const item = itemList[i];
+            const liElement = document.createElement('li');
+            liElement.classList.add('nav-item');
+            const aElement = document.createElement('a');
+            aElement.classList.add('nav-link');
+            aElement.setAttribute('data-bs-toggle', 'tab');
+            aElement.setAttribute('href', '#tab-' + i);
+            aElement.textContent = item.name;
+            liElement.appendChild(aElement);
+            llista_franquicies.appendChild(liElement);
+        }
+    } catch (error) {
+        console.error('Hubo un error al cargar el archivo JSON', error);
+    }
+
+
+
+
+
+
+}
+
+async function llistaSupermercats() {
+    try {
+        const responseSupermercat = await fetch('/assets/js/supermercat.json');
+        const json = await responseSupermercat.json();
         const itemList = json.itemListElement;
         const container = document.querySelector('.supermercats-container');
         for (let i = 0; i < itemList.length; i++) {
@@ -27,7 +70,7 @@ async function llistaSupermercats() {
             container.appendChild(newElement);
         }
     }
-    catch(error){
+    catch (error) {
         console.error('Hubo un error al cargar el archivo JSON', error);
     }
 }
