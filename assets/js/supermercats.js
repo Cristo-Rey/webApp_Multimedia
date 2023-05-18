aef96438d74b34227dba0a7ec07fe7caaaeb3c76
Joan López Ferrer <trombonet12@gmail.com>
2023-05-18 16:10:31 +0200

    Rentant sa merda d'en xavi

--- assets/js/supermercats.js
+++ assets/js/supermercats.js
@@ -6,91 +6,58 @@ async function llista_franquicies() {
         const llista_franquicies = document.getElementById('llista_franquicies');
         const div_franquicies = document.getElementById('div_franquicies');
 
-
-
-        /*
-                                    <div class="tab-pane active show" id="tab-1"> // Franquicia
-                                        <div class="row">   //row
-                                            <div class="col-lg-8 details order-2 order-lg-1"> // info
-                                                <h3>Architecto ut aperiam autem id</h3> // nom
-                                                <p class="fst-italic">Qui laudantium consequatur laborum sit qui ad sapiente 
-                                                    dila parde sonata raqer a videna mareta paulona marka</p> // desc
-                                                <p>Et nobis maiores eius. Voluptatibus ut enim blanditiis atque harum sint.
-                                                    Laborum eos ipsum ipsa odit magni. Incidunt hic ut molestiae aut qui. Est
-                                                    repellat minima eveniet eius et quis magni nihil. Consequatur dolorem
-                                                    quaerat quos qui similique accusamus nostrum rem vero</p>
-                                            </div>
-                                            <div class="col-lg-4 text-center order-1 order-lg-2">
-                                                <img src="assets/img/logo_franquicia/franquicia-1.jpg" alt="" class="img-fluid">
-                                            </div>
-                                        </div>
-                                    </div>
-        
-        */
-
-
-
-
-        //div_franquicies.appendChild(franquicia);
         for (let i = 0; i < itemList.length; i++) {
-                const item = itemList[i];
-                const liElement = document.createElement('li');
-                liElement.classList.add('nav-item');
-                const aElement = document.createElement('a');
-                aElement.classList.add('nav-link');
-                if(i==0){
-                    aElement.classList.add('active', 'show');
-                }
-                aElement.setAttribute('data-bs-toggle', 'tab');
-                aElement.setAttribute('href', '#tab-' + i);
-                aElement.textContent = item.name;
-                liElement.appendChild(aElement);
-                llista_franquicies.appendChild(liElement);
+            const item = itemList[i];
+            const liElement = document.createElement('li');
+            liElement.classList.add('nav-item');
+            const aElement = document.createElement('a');
+            aElement.classList.add('nav-link');
+            if (i == 0) {
+                aElement.classList.add('active', 'show');
+            }
+            aElement.setAttribute('data-bs-toggle', 'tab');
+            aElement.setAttribute('href', '#tab-' + i);
+            aElement.textContent = item.name;
+            liElement.appendChild(aElement);
+            llista_franquicies.appendChild(liElement);
 
-                const franquicia = document.createElement('div');
-                franquicia.classList.add("tab-pane");
-                if(i==0){
-                    franquicia.classList.add('active', 'show');
-                }
-                franquicia.setAttribute('id', "tab-" + i);
-                const row = document.createElement('div');
-                row.classList.add('row');
+            const franquicia = document.createElement('div');
+            franquicia.classList.add("tab-pane");
+            if (i == 0) {
+                franquicia.classList.add('active', 'show');
+            }
+            franquicia.setAttribute('id', "tab-" + i);
+            const row = document.createElement('div');
+            row.classList.add('row');
 
-                const info = document.createElement('div');
-                info.classList.add('col-lg-8', 'details', 'order-2', 'order-lg-1');
-                const nom = document.createElement('h3');
-                nom.textContent = itemList[i].name;
-                const desc = document.createElement('p');
-                desc.classList.add('fsc-italic');
-                desc.textContent = itemList[i].description;
+            const info = document.createElement('div');
+            info.classList.add('col-lg-8', 'details', 'order-2', 'order-lg-1');
+            const nom = document.createElement('h3');
+            nom.textContent = itemList[i].name;
+            const desc = document.createElement('p');
+            desc.classList.add('fsc-italic');
+            desc.textContent = itemList[i].description;
 
-                const imgdiv = document.createElement('div');
-                imgdiv.classList.add('col-lg-4', 'text-center', 'order-1', 'order-lg-2');
-                const img = document.createElement('img');
-                img.src = itemList[i].logo;
-                img.alt = '';
-                img.classList.add('img-fluid');
+            const imgdiv = document.createElement('div');
+            imgdiv.classList.add('col-lg-4', 'text-center', 'order-1', 'order-lg-2');
+            const img = document.createElement('img');
+            img.src = itemList[i].logo;
+            img.alt = '';
+            img.classList.add('img-fluid');
 
-                info.appendChild(nom);
-                info.appendChild(desc);
-                row.appendChild(info);
+            info.appendChild(nom);
+            info.appendChild(desc);
+            row.appendChild(info);
 
-                imgdiv.appendChild(img);
-                franquicia.appendChild(row);
-                row.appendChild(imgdiv);
-                div_franquicies.appendChild(franquicia);
-            
+            imgdiv.appendChild(img);
+            franquicia.appendChild(row);
+            row.appendChild(imgdiv);
+            div_franquicies.appendChild(franquicia);
+
         }
-        //div_franquicies.appendChild(franquicia);
     } catch (error) {
         console.error('Hubo un error al cargar el archivo JSON', error);
     }
-
-
-
-
-
-
 }
 
 async function llistaSupermercats() {
